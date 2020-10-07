aba45b81a86b29f72c32b0359bd1c63c95aff418
krzysztof <misiek.nalepa@wp.pl>
2020-10-07 13:22:37 +0200

    init

---
+++ src/pl/kraleppa/lab1/task_1/Counter.java
@@ -0,0 +1,4 @@
+package pl.kraleppa.lab1.task_1;
+
+public class Counter {
+}
---
+++ src/pl/kraleppa/lab1/task_1/Main.java
@@ -0,0 +1,8 @@
+package pl.kraleppa.lab1.task_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
