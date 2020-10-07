b6bfe6149494a9d4d1627a617937a40edf78469e
krzysztof <misiek.nalepa@wp.pl>
2020-10-07 13:32:00 +0200

    Counter without synchronization finished

--- src/pl/kraleppa/lab1/task_1/Counter.java
+++ src/pl/kraleppa/lab1/task_1/Counter.java
@@ -1,4 +1,18 @@
 package pl.kraleppa.lab1.task_1;
 
 public class Counter {
+    private int counter = 0;
+
+    public void increment(){
+        counter -= 1;
+    }
+
+    public void decrement(){
+        counter += 1;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(counter);
+    }
 }
--- src/pl/kraleppa/lab1/task_1/Main.java
+++ src/pl/kraleppa/lab1/task_1/Main.java
@@ -3,6 +3,22 @@ package pl.kraleppa.lab1.task_1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Counter counter = new Counter();
+	    Thread incrementer = new Thread(() -> {
+	        for (int i = 1; i < 100000000; i++){
+	            counter.increment();
+            }
+        });
+
+        Thread decrementer = new Thread(() -> {
+            for (int i = 1; i < 100000000; i++){
+                counter.decrement();
+            }
+        });
+
+        decrementer.start();
+        incrementer.start();
+
+        System.out.println(counter);
     }
 }
